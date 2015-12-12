8556c9abc00ada3df6199d0e8912802f94d67abe
Ryan Oliver <ROliver@zghealth.com>
2015-12-12 12:51:20 -0500

    Included .filter() test

--- 16-findersKeepers.js
+++ 16-findersKeepers.js
@@ -1,11 +1,21 @@
 function find(arr, func) {
   
- arr = arr.filter(function(val) {
-   return func(val);
- });
-  
-  return arr[0];
+ // declare global num variable
+  var num;
+
+  // Loop thorugh the array and use the function to check
+  for (var a = 0; a < arr.length; a++) {
+    if (func(arr[a])) {
+      // Store the first case and break the loop
+      num = arr[a];
+      return num;
+    }
+  }
+
+  // otherwise return undefined
+  return num;
 }
+
 
 //.filter() is not necessary for this simple exercise.  All we need to do is iterate through arr and 
 //  find the first value that returns true for the test function.  Set that first value equal to num 
